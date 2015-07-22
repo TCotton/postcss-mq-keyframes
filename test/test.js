4d850b2d368b6beb3f76895d2e3d45f105bcde14
TCotton <me@andywalpole.me>
2015-07-22 20:47:10 +0100

    added TDD tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
+/*eslint max-len:0*/
 var postcss = require('postcss');
 var expect = require('chai').expect;
-
+var assert = require('chai').assert;
+var fs = require('fs');
 var plugin = require('../');
 
-var test = function(input, output, opts, done) {
+function readCSS() {
+    return fs.readFileSync('./test/test.css', 'utf8');
+}
+
+var test = function test(input, output, opts, done) {
     postcss([plugin(opts)]).process(input).then(function(result) {
-        expect(result.css).to.eql(output);
+        assert.notEqual(result.css, output, 'these two CSS files are not equal');
+        assert.strictEqual(result.css.match(new RegExp('keyframes', 'g') || []).length, output.match(new RegExp('keyframes', 'g') || []).length, 'must be same number of keyframes');
         expect(result.warnings()).to.be.empty;
         done();
     }).catch(function(error) {
@@ -13,12 +20,10 @@ var test = function(input, output, opts, done) {
     });
 };
 
-describe('postcss-animations', function() {
+describe('postcss-animations - finding and moving keyframes to end of CSS file', function() {
 
-    // Write tests here
-
-    it('does something', function(done) {
-        test('a{ }', 'a{ }', {}, done);
+    it('Keyframe animation is now at end of the file', function(done) {
+        test(readCSS(), readCSS(), {}, done);
     });
 
 });
