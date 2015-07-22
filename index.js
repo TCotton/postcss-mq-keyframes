67f4e2541318c1f9034a20f0cccaa8fc51f59c93
TCotton <me@andywalpole.me>
2015-07-22 13:49:48 +0100

    more configuration files

---
+++ index.js
@@ -0,0 +1,13 @@
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('PLUGIN_NAME', function (opts) {
+  opts = opts || {};
+
+  // Work with options here
+
+  return function (css) {
+
+    // Transform CSS AST here
+
+  };
+});
